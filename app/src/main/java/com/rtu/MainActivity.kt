3df2e7f2ce0b3dcbff10dfef204fd6463d48973e
white <white8947@gmail.com>
2022-07-14 02:43:45 +0900

    Register View

--- app/src/main/java/com/rtu/MainActivity.kt
+++ app/src/main/java/com/rtu/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.rtu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.rtu.databinding.ActivityMainBinding
+import com.rtu.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    //private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    //private val viewModel: MainViewModel by lazy{MainViewModel()}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ViewDataBinding? = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        //val intent = Intent(this, RegisterActivity::class.java)
+        //startActivity(intent)
+
     }
 }
