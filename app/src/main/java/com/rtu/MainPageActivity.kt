976e1b3f5266f9111db5b733658f748890c945e3
white <white8947@gmail.com>
2022-07-24 02:09:16 +0900

    navigation bar

--- app/src/main/java/com/rtu/MainPageActivity.kt
+++ app/src/main/java/com/rtu/MainPageActivity.kt
@@ -3,9 +3,70 @@ package com.rtu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rtu.tab.GroupFragment
+import com.rtu.tab.HomeFragment
+import com.rtu.tab.MypageFragment
+import com.rtu.tab.RentFragment
+
 class MainPageActivity : AppCompatActivity() {
+    private val frame: ConstraintLayout by lazy { // activity_main의 화면 부분
+        findViewById(R.id.body_container)
+    }
+    private val bottomNagivationView: BottomNavigationView by lazy { // 하단 네비게이션 바
+        findViewById(R.id.bottom_navigation)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        // 애플리케이션 실행 후 첫 화면 설정
+        supportFragmentManager.beginTransaction().add(frame.id, HomeFragment()).commit()
+
+        // 하단 네비게이션 바 클릭 이벤트 설정
+        bottomNagivationView.setOnItemSelectedListener {item ->
+            when(item.itemId) {
+                R.id.nav_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_group -> {
+                    replaceFragment(GroupFragment())
+                    true
+                }
+                R.id.nav_rent -> {
+                    replaceFragment(RentFragment())
+                    true
+                }
+                R.id.nav_mypage -> {
+                    replaceFragment(MypageFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+    // 화면 전환 구현 메소드
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(frame.id, fragment).commit()
+    }
+
+    //백 버튼 이벤트
+    var backKeyPressedTime : Long=0
+    override fun onBackPressed() {
+        if(System.currentTimeMillis() > backKeyPressedTime+2500){
+            backKeyPressedTime=System.currentTimeMillis()
+            return
+        }
+
+        else{
+            finishAffinity()
+        }
     }
 }
---
+++ app/src/main/java/com/rtu/tab/GroupFragment.kt
@@ -0,0 +1,63 @@
+package com.rtu.tab
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.fragment.app.findFragment
+import com.google.android.material.tabs.TabLayout
+import com.rtu.R
+import com.rtu.databinding.FragmentGroupBinding
+import com.rtu.grouptap.GroupListFragment
+import com.rtu.grouptap.NoticeFragment
+
+class GroupFragment : Fragment() {
+
+
+    private var _binding: FragmentGroupBinding?=null
+
+    private val binding get()=_binding!!
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var frameLayout: FrameLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentGroupBinding.inflate(inflater, container, false)
+
+        frameLayout = binding.groupList
+
+        replaceFragment(GroupListFragment())
+
+        tabLayout = binding.tabLayout
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab!!.position) {
+                    0 -> replaceFragment(GroupListFragment())
+                    1 -> replaceFragment(NoticeFragment())
+                }
+            }
+        })
+
+        return binding.root
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction().replace(R.id.group_list, fragment)
+            .addToBackStack(null).commit()
+    }
+
+}
